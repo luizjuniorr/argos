69eb87f365d415e91cc79f57e1a7218566dbadbb
luizjuniorr <142500782+luizjuniorr@users.noreply.github.com>
2025-09-08 07:40:22 -0300

    Update TextoIntrucoes.jsx

--- src/components/TextoIntrucoes.jsx
+++ src/components/TextoIntrucoes.jsx
@@ -1,7 +1,7 @@
 export function TextoInstrucoes({texto}) {
     return(
         <div>
-            <p className="text-white text-sm md:text-xl font-redhat my-10 ">{texto}</p>
+            <p className="text-white text-sm md:text-xl font-redhat my-10 mx-8 md:mx-0">{texto}</p>
         </div>
     )
 }
