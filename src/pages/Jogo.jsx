d2535e4d43afc7700b9859a5a5852571e704f9ea
luizjuniorr <luiz.fernando.junior@eaportal.org>
2025-09-09 20:45:08 -0300

    Pagina do jogo arrumado

--- src/pages/Jogo.jsx
+++ src/pages/Jogo.jsx
@@ -15,9 +15,9 @@ export function Jogo() {
 
             <div className="flex flex-col md:flex-row h-screen justify-center items-center gap-16 max-w-6xl mx-auto py-16 px-4 ">
                 <div className="flex flex-col justify-center items-center md:items-start w-full md:w-1/2">
-                    <div className="flex flex-col md:flex-row justify-center items-center gap-3 mb-6">
-                        <h1 className="font-strike text-dark-green text-6xl text-center md:text-left dark:text-white">Argos Game</h1>
-                        <img src={theme === "dark" ? icon_jogo_escuro : icon_jogo} alt="" className="w-12" />
+                    <div className="flex justify-center items-center gap-3 mb-6">
+                        <h1 className="font-strike text-dark-green text-3xl md:text-6xl text-center md:text-left dark:text-white">Argos Game</h1>
+                        <img src={theme === "dark" ? icon_jogo_escuro : icon_jogo} alt="" className="w-8 md:w-12" />
                     </div>
 
                     <p className="text-sm font-redhat text-dark-green text-center md:text-left mb-6 dark:text-white">
@@ -26,7 +26,7 @@ export function Jogo() {
 
                     <img src={areareservada} alt="" className="w-full max-w-md mb-4 md:hidden border" />
 
-                    <button className="bg-dark-green w-3/5 md:w-full rounded-xl text-white font-strike cursor-pointer">Jogar</button>
+                    <button className="bg-dark-green w-3/5 h-8 md:w-full rounded-xl text-white font-strike cursor-pointer">Jogar</button>
                 </div>
 
                 <div className="w-full md:w-1/2 justify-center hidden md:flex">
