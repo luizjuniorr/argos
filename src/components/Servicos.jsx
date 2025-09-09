06c1382825948fc43cef0d7731fae285b79f3cec
luizjuniorr <luiz.fernando.junior@eaportal.org>
2025-09-09 17:08:26 -0300

    Tema claro e escuro finalizado

--- src/components/Servicos.jsx
+++ src/components/Servicos.jsx
@@ -1,9 +1,13 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext.jsx";
+
 export function Servicos({ImagemEscuro, ImagemClaro, Titulo, Texto}){
     const tema = localStorage.getItem("tema");
+    const { theme, toggleTheme } = useContext(ThemeContext)
 
     return (
         <div className="flex items-center justify-center gap-4">
-            <img src={tema === "dark" ? ImagemEscuro : ImagemClaro} alt="Ícone" className="h-14" />
+            <img src={theme === "dark" ? ImagemEscuro : ImagemClaro} alt="Ícone" className="h-14" />
 
             <div className="text-center">
                 <h3 className="font-strike md:text-xl md:font-gabarito">{Titulo}</h3>
--- src/pages/Jogo.jsx
+++ src/pages/Jogo.jsx
@@ -1,9 +1,14 @@
 import { Header } from "../components/Header";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext.jsx";
 
 import icon_jogo from "../assets/icon_jogo.png";
+import icon_jogo_escuro from "../assets/icon_jogo_escuro.png"
 import areareservada from "../assets/areareservada.png";
 
 export function Jogo() {
+    const { theme, toggleTheme } = useContext(ThemeContext)
+
     return (
         <div className="dark:text-white">
             <Header />
@@ -12,7 +17,7 @@ export function Jogo() {
                 <div className="flex flex-col justify-center items-center md:items-start w-full md:w-1/2">
                     <div className="flex flex-col md:flex-row justify-center items-center gap-3 mb-6">
                         <h1 className="font-strike text-dark-green text-6xl text-center md:text-left dark:text-white">Argos Game</h1>
-                        <img src={icon_jogo} alt="" className="w-12" />
+                        <img src={theme === "dark" ? icon_jogo_escuro : icon_jogo} alt="" className="w-12" />
                     </div>
 
                     <p className="text-sm font-redhat text-dark-green text-center md:text-left mb-6 dark:text-white">
